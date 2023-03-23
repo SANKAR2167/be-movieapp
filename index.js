2bd8fa1a68a96aed86d655943e8349f956e028ad
SANKAR2167 <snakarkulandaivel@gmail.com>
2023-03-23 11:40:14 +0530

    routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import express from "express"; // "type": "module"
 import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv";
+import moviesRouter from "./routes/movies.route.js";
+
 dotenv.config()
 const app = express();
 
@@ -21,61 +23,8 @@ app.get("/", function (request, response) {
     response.send(`Hello World !!!`);
 });
 
-app.get("/movies", async function (request, response) {
+app.use("/movies", moviesRouter)
 
-    if (request.query.rating) {
-        request.query.rating = +request.query.rating;
-    }
-
-    const movie = await client
-        .db("movieapp")
-        .collection("movies")
-        .find(request.query)
-        .toArray();
-    response.send(movie);
-});
-
-app.get("/movies/:id", async function (request, response) {
-    const { id } = request.params;
-    const movie = await client.db("movieapp").collection("movies").findOne({ id: id })
-    //const movie = movies.find((mv) => mv.id === id);
-    console.log(movie);
-    movie ? response.send(movie) : response.status(404).send({ message: "movie not found" })
-});
-
-app.post("/movies", async function (request, response) {
-    const data = request.body;
-    console.log(data);
-    const result = await client
-        .db("movieapp")
-        .collection("movies")
-        .insertMany(data);
-    response.send(result);
-});
-
-app.delete("/movies/:id", async function (request, response) {
-    const { id } = request.params;
-    const result = await client
-        .db("movieapp")
-        .collection("movies")
-        .deleteOne({ id: id });
-
-    result.deletedCount > 0
-        ? response.send({ message: "movie deleted successfully" })
-        : response.status(404).send({ message: "movie not found" });
-})
-
-
-app.put("/movies/:id", async function (request, response) {
-    const { id } = use.params;
-    const data = request.body;
-
-    const result = await client
-        .db("movieapp")
-        .collection("movies")
-        .updateOne({ id: id }, { $set: data });
-
-    console.log(result);
-    response.send(result);
-});
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
+
+export {client}
---
+++ routes/movies.route.js
@@ -0,0 +1,63 @@
+import express from "express";
+import { client } from "../index.js";
+
+const router = express.Router()
+
+router.get("/", async function (request, response) {
+
+    if (request.query.rating) {
+        request.query.rating = +request.query.rating;
+    }
+
+    const movie = await client
+        .db("movieapp")
+        .collection("movies")
+        .find(request.query)
+        .toArray();
+    response.send(movie);
+});
+
+router.get("/:id", async function (request, response) {
+    const { id } = request.params;
+    const movie = await client.db("movieapp").collection("movies").findOne({ id: id })
+    //const movie = movies.find((mv) => mv.id === id);
+    console.log(movie);
+    movie ? response.send(movie) : response.status(404).send({ message: "movie not found" })
+});
+
+router.post("", async function (request, response) {
+    const data = request.body;
+    console.log(data);
+    const result = await client
+        .db("movieapp")
+        .collection("movies")
+        .insertMany(data);
+    response.send(result);
+});
+
+router.delete("/:id", async function (request, response) {
+    const { id } = request.params;
+    const result = await client
+        .db("movieapp")
+        .collection("movies")
+        .deleteOne({ id: id });
+
+    result.deletedCount > 0
+        ? response.send({ message: "movie deleted successfully" })
+        : response.status(404).send({ message: "movie not found" });
+})
+
+router.put("/:id", async function (request, response) {
+    const { id } = use.params;
+    const data = request.body;
+
+    const result = await client
+        .db("movieapp")
+        .collection("movies")
+        .updateOne({ id: id }, { $set: data });
+
+    console.log(result);
+    response.send(result);
+});
+
+export default router
